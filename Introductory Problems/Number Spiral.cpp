a1f5f3ec650210f8b05dfef2060a14e2e99d8c81
Shayak <44469461+shr0nin@users.noreply.github.com>
2020-07-04 18:17:06 +0530

    added one more solution

--- Introductory Problems/Number Spiral.cpp
+++ Introductory Problems/Number Spiral.cpp
@@ -1,38 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int nax=1e9+5;
-vector<vector<long long>> a(nax, vector<long long> (nax)); 
-
-void init() {
-    a[1][1]=1;
-    a[1][2]=2;
-    a[2][1]=3;
-    a[2][2]=4;
-    long long val=5;
-    int row=3, col=3, now=2, dir=0;
-    while(row<nax && col<nax) {
-        if(dir)
-        else {
-            int i=row,j;
-            for(j=1; j<col; ++j) {
-                a[i][j]=val++;
-            }
-        }
-    }
-}
-
-void Main() {
-    int r,c;
-    scanf("%d%d", &r,&c);
-}
-
 int main() {
-    
     int T;
     scanf("%d", &T);
     while(T--) {
-        Main();
+        int r,c;
+        scanf("%d%d", &r,&c);
+        long long ans;
+        if(r<c) {
+            if(c%2) 
+                ans=1LL*c*c-(r-1);
+            else 
+                ans=1LL*(c-1)*(c-1)+1+(r-1);
+        }
+        else {
+            if(r%2==0) 
+                ans=1LL*r*r-(c-1);
+            else 
+                ans=1LL*(r-1)*(r-1)+1+(c-1);
+        }
+        printf("%lld\n", ans);
     }
 
     return 0;
