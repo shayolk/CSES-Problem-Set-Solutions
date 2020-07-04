5364327c6067c21b5f50a9f0923d86864b1cabdc
Shayak <44469461+shr0nin@users.noreply.github.com>
2020-07-04 17:28:37 +0530

    fixed spacing

--- Introductory Problems/Permutations.cpp
+++ Introductory Problems/Permutations.cpp
@@ -14,21 +14,21 @@ int main() {
     }
     if(n%4==0) {
         for(int i=1; i<n; i+=4) {
-        	printf("%d %d %d %d ", i+1,i+3,i,i+2);
+            printf("%d %d %d %d ", i+1,i+3,i,i+2);
         }
         return 0;
     }
     if(n%4==1) {
         printf("1 ");
         for(int i=2; i<n; i+=4) {
-        	printf("%d %d %d %d ", i+1,i+3,i,i+2);
+            printf("%d %d %d %d ", i+1,i+3,i,i+2);
         }
         return 0;
     }
     if(n%4==2) {
         printf("1 4 2 5 3 6 ");
         for(int i=7; i<n-1; i+=4) {
-        	printf("%d %d %d %d ", i+1,i+3,i,i+2);
+            printf("%d %d %d %d ", i+1,i+3,i,i+2);
         }
         return 0;
     }
